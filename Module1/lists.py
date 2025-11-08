a3d85a84283b7c07a765e86b1be50011ef621591
Liridona Baliu <liridona.baliu@digitalschool.tech>
2025-11-08 14:33:10 +0100

    lesson1

---
+++ Module1/lists.py
@@ -0,0 +1,41 @@
+to_do_list = ["math homework","clean the room" ,"research for history", "fun"]
+
+print(to_do_list[0])
+print(to_do_list[1])
+print(to_do_list[2])
+print(to_do_list[3])
+
+shopping_list = ["apples" , "bananas", 3, 8]
+shopping_list.append("oranges")
+
+print(shopping_list)
+
+shopping_list.insert(2,"lemons")
+print(shopping_list)
+
+to_do_list.remove("research for history")
+print(to_do_list)
+
+del to_do_list[2]
+print(to_do_list)
+
+to_do_list[0] ="physics homework"
+print(to_do_list)
+
+to_do_list[1] = 3
+print(to_do_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

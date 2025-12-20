1113c53b19aa834a0f038a6134adf4564955e361
Liridona Baliu <liridona.baliu@digitalschool.tech>
2025-12-20 14:33:02 +0100

    lesson 6 and 7

---
+++ Module6/main.py
@@ -0,0 +1,11 @@
+# First way
+import my_math
+
+result = my_math.square(5)
+print(result)
+
+# Second way
+from my_math import square
+result = square(4)
+print(result)
+
---
+++ Module6/main3.py
@@ -0,0 +1,6 @@
+import importlib
+
+import my_module
+
+#Reload the module using importlib to upadate changes made to it
+importlib.reload(my_module)
